edf3eca617bc59fc50fad1d90627158aa859a47d
helenabrunel <helena.brunel@gmail.com>
2018-04-13 13:40:55 +0100

    Update and rename GATKcallingscript.py to GATKHCscript.py

--- 5.VC/GATKHCscript.py
+++ 5.VC/GATKHCscript.py
@@ -15,24 +15,25 @@ f1 = sys.argv[2]
 outdir = sys.argv[3]
 
 #GATK
-
+#index bam file
 cmnd = "samtools_mt  index " + f1
 print(cmnd)
 os.system(cmnd)
 
-ref = "/mnt/DATA/home4/arc/hb493/b37/Ensembl_GRCh37.ordered.fa"
+#variant calling
+ref = "Ensembl_GRCh37.ordered.fa"
 out = outdir + "/" + basename + ".GATK.vcf"
-cmnd = "java -jar /mnt/DATA/home4/arc/hb493/bin/GenomeAnalysisTK.jar -R " + ref + " -T HaplotypeCaller -I " + f1 + " -o " + out
+cmnd = "java -jar GenomeAnalysisTK.jar -R " + ref + " -T HaplotypeCaller -I " + f1 + " -o " + out
 print(cmnd)
 os.system(cmnd)
 
-cmnd = "sed -i '/^#/ s/20$/LP2100082-DNA_B03/g' " + out
+#change sample name in vcf file
+cmnd = "sed -i '/^#/ s/20/" + basename + "/g' " + out
 print(cmnd)
 os.system(cmnd)
 
-
+#index vcf file
 out2 =  outdir + "/" + basename + ".GATK.vcf.gz"
-
 cmnd = "bgzip -c " + out + " > " + out2
 print(cmnd)
 os.system(cmnd)
