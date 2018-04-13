611aebc4f74ac6b06d2720761920dbd8e5c4d69a
helenabrunel <helena.brunel@gmail.com>
2018-04-13 12:57:48 +0100

    Update submitCONVERT.py

--- 0.CONVERT/submitCONVERT.py
+++ 0.CONVERT/submitCONVERT.py
@@ -5,8 +5,8 @@ import os.path
 import re
 
 #define input and output folders
-indir =os.path.dirname("/mnt/DATA/home4/arc/hb493/GF0/input/")
-outdir = os.path.dirname("/mnt/DATA/home4/arc/hb493/GF0/output/fastq/")
+indir =os.path.dirname("path/indir")
+outdir = os.path.dirname("/path/outdir")
 
 # This will:
 #     - Identify the bam files in the input folder
@@ -28,8 +28,8 @@ for file in os.listdir(indir):
 # Run CONVERTscript.py parallely for each file
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
-	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/CONVERTscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-	print "sbatch --partition=LONG /mnt/DATA/home4/arc/hb493/bin/submit_sbatch0.sh"
+	print "export SBATCH_CMD=\"python CONVERTscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+	print "sbatch --partition=LONG submit_sbatch0.sh"
 	print "export SBATCH_CMD=\"\""
 
 
