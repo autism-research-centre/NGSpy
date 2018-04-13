f2417e167c9ade3c94562564428f8426d8486a6c
helenabrunel <helena.brunel@gmail.com>
2018-04-13 13:15:48 +0100

    Update submitPICARDscript.py

--- 3.PICARD/submitPICARDscript.py
+++ 3.PICARD/submitPICARDscript.py
@@ -5,11 +5,9 @@ import os.path
 import re
 
 #define input and output folders
-#indir =os.path.dirname("/mnt/DATA/home4/arc/hb493/GF7/output/b37/BWAMEM/")
-#outdir = os.path.dirname("/mnt/DATA/home4/arc/hb493/GF7/output/b37/BWAMEM/PICARD/")
+indir =os.path.dirname("path/indir") # BWAMEM output dir
+outdir = os.path.dirname("path/outdir")
 
-indir = os.path.dirname("/mnt/b2/home4/arc/hb493/Rimma_fastq/PICARD/input/")
-outdir = os.path.dirname("/mnt/b2/home4/arc/hb493/Rimma_fastq/PICARD/output/")
 bnlist=[]
 r=[]
 # identify mapped.sorted.bam files
@@ -25,8 +23,8 @@ for file in os.listdir(indir):
 
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
-	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/PICARDscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-	print "sbatch --partition=LONG /mnt/DATA/home4/arc/hb493/bin/submit_sbatch3.sh"
+	print "export SBATCH_CMD=\"python PICARDscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+	print "sbatch --partition=LONG submit_sbatch3.sh"
 	print "export SBATCH_CMD=\"\""
 
 #	print ("python BOWTIEscript.py " + bnlist[i] + " " + r1[i] + " " + r2[i])
