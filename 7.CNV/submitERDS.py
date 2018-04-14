9786f535a5e77c19d838b76af4215b30355bfbf4
helenabrunel <helena.brunel@gmail.com>
2018-04-14 22:35:52 +0100

    Update submitERDS.py

--- 7.CNV/submitERDS.py
+++ 7.CNV/submitERDS.py
@@ -5,11 +5,10 @@ import os.path
 import re
 
 #define input and output folders
-indir =os.path.dirname("/mnt/DATA/home4/arc/hb493/GF20/output/b37/BWAMEM/GATK/")
-outdir = os.path.dirname("/mnt/DATA/home4/arc/hb493/GF20/output/b37/CNV/erds_GATK/")
+indir =os.path.dirname("path/indir") #output from GATK step
+outdir = os.path.dirname("path/outdir")
 
-# indir =os.path.dirname("/mnt/b2/home4/arc/hb493/Rimma_fastq/PICARD/output/")
-# outdir = os.path.dirname("/mnt/b2/home4/arc/hb493/Rimma_fastq/CNV/erds/")
+
 
 bnlist=[]
 r=[]
@@ -26,19 +25,6 @@ for file in os.listdir(indir):
 
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
-	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/pipB37/ERDSscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-	print "sbatch --partition=ERDS /mnt/DATA/home4/arc/hb493/bin/submit_sbatch14.sh"
+	print "export SBATCH_CMD=\"python ERDSscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+	print "sbatch --partition=ERDS submit_sbatch14.sh"
 	print "export SBATCH_CMD=\"\""
-
-# for i in range(len(bnlist)):
-# 	print "export SBATCH_CMD=\"\""
-# 	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/PINDELscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-# 	print "sbatch --partition=1604 /mnt/DATA/home4/arc/hb493/bin/submit_sbatch10.sh"
-# 	print "export SBATCH_CMD=\"\""
-# 
-# 
-# for i in range(len(bnlist)):
-# 	print "export SBATCH_CMD=\"\""
-# 	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/DELLYscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-# 	print "sbatch --partition=1604 /mnt/DATA/home4/arc/hb493/bin/submit_sbatch11.sh"
-# 	print "export SBATCH_CMD=\"\""
