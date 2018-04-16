95aec15aa86b1840aa968821c00de96352634a55
helenabrunel <helena.brunel@gmail.com>
2018-04-16 11:47:24 +0100

    Update submitSVCALLINGscript.py

--- 8.SV/submitSVCALLINGscript.py
+++ 8.SV/submitSVCALLINGscript.py
@@ -5,11 +5,9 @@ import os.path
 import re
 
 #define input and output folders
-indir =os.path.dirname("/mnt/DATA/home4/arc/hb493/Rimma_fastq/GATK/output/")
-outdir = os.path.dirname("/mnt/DATA/home4/arc/hb493/Rimma_fastq/SV/")
+indir = os.path.dirname("path/indir/") # output from the GATK step
+outdir = os.path.dirname("path/outdir") 
 
-#input_dir = os.path.dirname(/mnt/DATA/home4/arc/hb493/Rimma_fastq)
-#output_dir = os.path.dirname(/mnt/DATA/home4/arc/hb493/Rimma_fastq)
 bnlist=[]
 r=[]
 # identify mapped.sorted.bam files
@@ -25,19 +23,21 @@ for file in os.listdir(indir):
 
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
-	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/BreakDANCERscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+	print "export SBATCH_CMD=\"python BreakDANCERscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
 	print "sbatch --partition=1604 /mnt/DATA/home4/arc/hb493/bin/submit_sbatch6.sh"
 	print "export SBATCH_CMD=\"\""
 
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
-	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/PINDELscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-	print "sbatch --partition=1604 /mnt/DATA/home4/arc/hb493/bin/submit_sbatch10.sh"
+	print "export SBATCH_CMD=\"python PINDELscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+	print "sbatch --partition=1604 submit_sbatch10.sh"
 	print "export SBATCH_CMD=\"\""
 
 
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
-	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/DELLYscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
-	print "sbatch --partition=1604 /mnt/DATA/home4/arc/hb493/bin/submit_sbatch11.sh"
+	print "export SBATCH_CMD=\"python DELLYscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+	print "sbatch --partition=1604 submit_sbatch11.sh"
 	print "export SBATCH_CMD=\"\""
+	
+	
