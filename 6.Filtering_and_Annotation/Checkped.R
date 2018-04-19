b588cd7550ebe558e630660d48637eb9407a2ddf
helenabrunel <helena.brunel@gmail.com>
2018-04-19 17:02:29 +0100

    Update Checkped.R
    Add BAP MAP NAP

--- 6.Filtering_and_Annotation/Checkped.R
+++ 6.Filtering_and_Annotation/Checkped.R
@@ -13,8 +13,12 @@ if (length(args)!=2){
 #Identify gene files
 gene_files <- list.files(path = indir, pattern = "*.ExAC.xls")
 
-#define col names for result table  Aff_r is the rate of affected individuals with the variants (within the family) and NonAff_r is the rate of non-affected individuals with the variants (within the family) 
+#define col names for result table  Aff_r is the rate of affected individuals with the variants (within the family) and NonAff_r is the rate of non-affected individuals with the variants (within the family)
+#For BAP MAP NAP rates are for variants exclusive to each category
+#For diagnosis
 var_table <- c("Vchr", "Vpos", "snp_Eff", "Aff_r", "NonAff_r")
+#For BAP/MAP/NAP
+var_table <- c("Vchr", "Vpos", "snp_Eff", "var_BAP_r", "var_MAP_r", "var_NAP_r")
 
 #for each gene, read the file and for every unique variant, match with the pedigree and count variants present in affected and non-affected subjects
 
@@ -37,9 +41,16 @@ for (i in c(1:length(gene_files)))
       var_chr <- unique(variant_table$chr)
       var_pos <- unique(variant_table$pos)
       var_snpEff <- as.character(variant_table$snpEff[1])
-      var_Aff_r <- sum(variant_table$V6==2)/sum(ped$V6==2)
-      var_nonAff_r <- sum(variant_table$V6==1)/sum(ped$V6==1)
-      var_table<- rbind(var_table, c(var_chr, var_pos, var_snpEff, var_Aff_r, var_nonAff_r))
+      # For diagnosis
+      #var_Aff_r <- sum(variant_table$V6==2)/sum(ped$V6==2)
+      #var_nonAff_r <- sum(variant_table$V6==1)/sum(ped$V6==1)
+      #var_table<- rbind(var_table, c(var_chr, var_pos, var_snpEff, var_Aff_r, var_nonAff_r))
+      
+      #For BAP/MAP/NAP
+      var_BAP_r <- sum(variant_table$V6=="BAP")/sum(ped$V6=="BAP")
+      var_MAP_r <- sum(variant_table$V6=="MAP")/sum(ped$V6=="MAP")
+      var_NAP_r <- sum(variant_table$V6=="NAP")/sum(ped$V6=="NAP")
+      var_table<- rbind(var_table, c(var_chr, var_pos, var_snpEff, var_BAP_r, var_MAP_r, var_NAP_r))
       }
     }
 }
@@ -48,16 +59,34 @@ if (!is.null(dim(var_table)))
 {
   vt <- as.data.frame(var_table)
   vt <- vt[-1,]
-  names(vt) <- c("Vchr", "Vpos", "snpEff", "Aff_r", "NonAff_r")
+  # For diagnosis
+  # names(vt) <- c("Vchr", "Vpos", "snpEff", "Aff_r", "NonAff_r")
+  ##For BAP/MAP/NAP
+  names(vt) <- c("Vchr", "Vpos", "snpEff", "BAP", "MAP", "NAP")
   write.table(vt, file=paste(indir, "/AV_all.txt", sep=""), row.names=F,col.names=T, quote=F)
+
+# For diagnosis
+#df1 <- subset(vt, Aff_r==1)
+#df2 <- subset(df1, NonAff_r==0)
+
+#For BAP/MAP/NAP
+df1 <- subset(vt, BAP==1 & MAP==0 & NAP==0)
+df2 <- subset(vt, MAP==1 & BAP==0 & NAP==0)
+df3 <- subset(vt, NAP==1& MAP==0 & BAP==0)
   
-  df1 <- subset(vt, Aff_r==1)
-  df2 <- subset(df1, NonAff_r==0)
-  if(dim(df1)[1]>0)
-    write.table(df1, file=paste(indir, "/AV_aff.txt", sep=""), row.names=F, col.names=T, quote=F)
+# For diagnosis	
+#  if(dim(df1)[1]>0)
+#    write.table(df1, file=paste(indir, "/AV_aff.txt", sep=""), row.names=F, col.names=T, quote=F)
+#  if(dim(df2)[1]>0)
+#    write.table(df2, file=paste(indir, "/AV_allAff.txt", sep=""), row.names=F, col.names=T, quote=F)
+  
+#For BAP/MAP/NAP
+if(dim(df1)[1]>0)
+    write.table(df1, file=paste(indir, "/AV_BAP.txt", sep=""), row.names=F, col.names=T, quote=F)
   if(dim(df2)[1]>0)
-    write.table(df2, file=paste(indir, "/AV_allAff.txt", sep=""), row.names=F, col.names=T, quote=F)
-  
+    write.table(df2, file=paste(indir, "/AV_MAP.txt", sep=""), row.names=F, col.names=T, quote=F)
+  if(dim(df3)[1]>0)
+    write.table(df3, file=paste(indir, "/AV_NAP.txt", sep=""), row.names=F, col.names=T, quote=F)	
 }
 
 
