557d57acdc69d281f3b1b3cfca52ee4efdf1fa08
helenabrunel <helena.brunel@gmail.com>
2018-08-21 17:04:24 +0100

    Update submitPINDELscript.py

--- 8.SV/submitPINDELscript.py
+++ 8.SV/submitPINDELscript.py
@@ -5,11 +5,9 @@ import os.path
 import re
 
 #define input and output folders
-indir =os.path.dirname("/mnt/DATA/home4/arc/hb493/GF20/output/b37/BWAMEM/GATK/")
-outdir = os.path.dirname("/mnt/DATA/home4/arc/hb493/GF20/output/b37/SV/pindel/")
+indir =os.path.dirname("path/indir")
+outdir = os.path.dirname("path/outdir")
 
-#indir =os.path.dirname("/mnt/b2/home4/arc/hb493/Rimma_fastq/PICARD/output/")
-#outdir = os.path.dirname("/mnt/b2/home4/arc/hb493/Rimma_fastq/CNV/cnvnator")
 
 bnlist=[]
 r=[]
@@ -28,7 +26,7 @@ for file in os.listdir(indir):
 
 for i in range(len(bnlist)):
 	print "export SBATCH_CMD=\"\""
- 	print "export SBATCH_CMD=\"python /mnt/DATA/home4/arc/hb493/scripts/pipB37/PINDELscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
- 	print "sbatch --partition=MAPR /mnt/DATA/home4/arc/hb493/bin/submit_sbatch10.sh"
+ 	print "export SBATCH_CMD=\"python /path/PINDELscript.py " + bnlist[i] + " " + r[i] + " " + outdir + "\""
+ 	print "sbatch --partition=MAPR /path/submit_sbatch10.sh"
  	print "export SBATCH_CMD=\"\""
 
