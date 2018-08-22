147bd3145db9fb8b43fb75be54d2b1649a9fe7eb
helenabrunel <helena.brunel@gmail.com>
2018-08-22 12:16:06 +0100

    Update PINDELscript.py

--- 5. Variant Calling/SV/PINDELscript.py
+++ 5. Variant Calling/SV/PINDELscript.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python
 
+#!/usr/local/bin/python
+
 import os
 import sys
 import os.path
@@ -15,6 +17,7 @@ f1 = sys.argv[2]
 outdir = sys.argv[3]
 
 
+
 ref = "ref.fa"
 
 
@@ -28,10 +31,78 @@ f.close()
 
 # STEP 2 :. Run PINDEL
 out6 = outdir + "/" + basename + ".pindel" 
-cmnd = "pindel -f " + ref + " -i " + cfg + " -c ALL -o " + out6 
+cmnd = "path/pindel -f " + ref + " -i " + cfg + " -c ALL -o " + out6 + " -T 24"
 print(cmnd)
 os.system(cmnd)
 
+
+#STEP 3: OPTIONAL : convert to vcf
+
+#o1 = outdir + "/" + basename + ".pindel_D"
+#if not (os.stat(o1).st_size == 0):
+#	o1vcf = outdir + "/" + basename + ".pindel_D.vcf"
+#	cmnd = "path/pindel2vcf -p " + o1 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o1vcf
+#	print(cmnd)
+#	os.system(cmnd)
+
+#o2 = outdir + "/" + basename + ".pindel_SI"
+#if not (os.stat(o2).st_size == 0):
+#	o2vcf = outdir + "/" + basename + ".pindel_SI.vcf"
+#	cmnd = "/path/pindel2vcf -p " + o2 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o2vcf
+#	print(cmnd)
+#	os.system(cmnd)
+
+
+#o3 = outdir + "/" + basename + ".pindel_LI"
+#if not (os.stat(o3).st_size == 0):
+#	o3vcf = outdir + "/" + basename + ".pindel_LI.vcf"
+#	cmnd = "/path/pindel2vcf -p " + o3 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o3vcf
+#	print(cmnd)
+#	os.system(cmnd)
+	
+#o4 = outdir + "/" + basename + ".pindel_INV"
+#if not (os.stat(o4).st_size == 0):
+#	o4vcf = outdir + "/" + basename + ".pindel_INV.vcf"
+#	cmnd = "/path/pindel2vcf -p " + o4 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o4vcf
+#	print(cmnd)
+#	os.system(cmnd)	
+	
+# o5 = outdir + "/" + basename + ".pindel_TD"
+# if not (os.stat(o5).st_size == 0):
+# 	o5vcf = outdir + "/" + basename + ".pindel_TD.vcf"
+# 	cmnd = "path/pindel2vcf -p " + o5 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o5vcf
+# 	print(cmnd)
+# 	os.system(cmnd)
+# 
+# o6 = outdir + "/" + basename + ".pindel_BP"
+# if not (os.stat(o6).st_size == 0):
+# 	o6vcf = outdir + "/" + basename + ".pindel_BP.vcf"
+# 	cmnd = "patha/pindel2vcf -p " + o6 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o6vcf
+# 	print(cmnd)
+# 	os.system(cmnd)
+# 
+# o7 = outdir + "/" + basename + ".pindel_CloseEndMapped"
+# if not (os.stat(o7).st_size == 0):
+# 	o7vcf = outdir + "/" + basename + ".pindel_CloseEndMapped.vcf"
+# 	cmnd = "path/pindel2vcf -p " + o7 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o7vcf
+# 	print(cmnd)
+# 	os.system(cmnd)
+# 	
+# o8 = outdir + "/" + basename + ".pindel_INT_final"
+# if not (os.stat(o8).st_size == 0):
+# 	o8vcf = outdir + "/" + basename + ".pindel_INT_final.vcf"
+# 	cmnd = "path/pindel2vcf -p " + o8 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o8vcf
+# 	print(cmnd)
+# 	os.system(cmnd)
+# 	
+# o9 = outdir + "/" + basename + ".pindel_RP"
+# if not (os.stat(o9).st_size == 0):
+# 	o9vcf = outdir + "/" + basename + ".pindel_RP.vcf"
+# 	cmnd = "path/pindel2vcf -p " + o9 + " -r " + ref + "  -R hg19 -d 20131217 -v " + o9vcf
+# 	print(cmnd)
+# 	os.system(cmnd)	
+# 	
+	
 
 
 	
